c3f118bc54e994c6c1fa8ebe41c814998949acf4
Yuri Yu <4kmswinds@gmail.com>
2024-01-04 16:56:41 -0800

    fixed the display issues

--- utils/metadata.py
+++ utils/metadata.py
@@ -23,29 +23,29 @@ def metadata(df):
     metadata = pd.DataFrame(columns_list, columns=['col_name'])
 
     # Data types
-    metadata['data_type'] = df.dtypes.astype(str)
+    metadata['data_type'] = [i for i in df.dtypes.astype(str)]
 
     # Missing values
-    missing_values = df.isnull().sum()
-    metadata['missing_values'] = missing_values
+    metadata['missing_values'] = [i for i in df.isnull().sum()]
 
     # Missing values percentage
-    metadata['missing_values_percentage'] = round(missing_values * 100 / df.shape[0], 2)
+    metadata['missing_values_percentage'] =  [round(i*100,2) for i in df.isnull().sum() / df.shape[0]]
     
     # Number of unique values (numerical representation)
     unique_values_num = df.nunique()
-    metadata['unique_values_num'] = unique_values_num
+    metadata['unique_values_num'] = [i for i in unique_values_num]
 
-    # Number of unique values percentage
-    metadata['unique_values_percentage'] = round(unique_values_num * 100 / df.shape[0], 2)
+    # Number of unique values percentage [round(i*100,2) for i in df.nunique() / df.shape[0]]
+    metadata['unique_values_percentage'] = [round(i*100,2) for i in df.nunique() / df.shape[0]]
 
     # Unique values for columns with less than 10 unique values
-    metadata['unique_values'] = [df[col].unique() if unique_values_num[col] < 10 else 'NA' for col in columns_list]
+    metadata['unique_values'] = [df[col].unique() if unique_values_num[col] <= 10 else 'NA' for col in columns_list]
 
     # Basic statistics for interval columns (numerical types only)
-    interval_columns = [col for col in columns_list if df[col].dtype in ['int64', 'float64']]
+    interval_columns = [col for col in columns_list if df[col].dtype in ['int64', 'float64', 'int32', 'float32', 'int16', 'float16', 'int8', 'float8', 'int', 'float', 'double']]
     if interval_columns:
-        desc_interval = df[interval_columns].agg(['min', 'max', 'std']).transpose().reset_index().rename(columns={'index': 'col_name'})
+        desc_interval = df[interval_columns].describe().transpose().reset_index().rename(columns={'index': 'col_name'})
+        desc_interval = desc_interval[['col_name', 'min', 'max', 'std']]
         metadata = metadata.merge(desc_interval, on='col_name', how='left')
 
     return metadata
